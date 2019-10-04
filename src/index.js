b0e9c0b666838001464b6c220e9a1078e15494d9
Mykola Antypenko <mykola_antypenko@epam.com>
2019-10-04 16:39:33 +0300

    Commit with sorting to pageBook

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 
 
-import "E:/EPAM/PageBookProject/src/css/style.css";
+import "./css/style.css";
 import {sortable} from './js/sorting.js';
+import {hidden1} from './js/sorting.js';
+import {hidden2} from './js/sorting.js';
 import {animation_menu} from './js/animation.js';
 import {animation_close} from './js/animation.js';
 
 
 
-const sort = document.getElementById("lol");
-
+const sort = document.getElementById("popular");
 sort.addEventListener("click", () => sortable(".blocks", "data-views"));
+sort.addEventListener("click", hidden1);
+sort.addEventListener("click", hidden2);
 
 var menu = document.querySelector("#nav-toggle");
 	menu.addEventListener("change", animation_menu);
--- src/js/sorting.js
+++ src/js/sorting.js
@@ -1,10 +1,11 @@
 // sort by popular
 
 function sortable(cls, attr) {
-  console.log("lol in func");
+    
         var elements = document.querySelectorAll(cls),
             len = elements.length,
             parent = document.querySelector('.main-content'),
+            
             arrElements = [];
 
         for(var i = 0; i < len; i++) {
@@ -20,13 +21,36 @@ function sortable(cls, attr) {
 
         for(var j = 0; j < len; j++) {
             parent.appendChild(arrElements[j].el);
-            parent.style.width = "65%";
+            parent.style.width = "40%";
             parent.style.margin = "0 auto";
         }
-          console.log("Correct work")
+          console.log("Correct work");
       }
 
-      
+      function hidden1() {
+        var pict = document.getElementById('image_none1');
+        var subblocks = document.querySelector('.block-card');
+        var blockmargin = document.querySelector('.section-part2');
+        var alignblocks = document.querySelectorAll('.blocks');
+        var len = alignblocks.length;
+for (const block of alignblocks) {
+    block.style.marginBottom = "50px";
+}
+    
+        subblocks.style.width = "100%";
+        subblocks.style.height = "70%";
+        subblocks.style.right = "0";
+        pict.style.display = 'none';
+
+    }
+    function hidden2() {
+        var pict = document.getElementById('image_none2');
+        var subblocks = document.querySelector('.blockpart6-card');
+        var blockmargin = document.querySelector('.section-part6');
+        subblocks.style.width = "100%";
+        blockmargin.style.margin = "0";
+        pict.style.display = 'none';
+    }   
 
       
 
@@ -34,3 +58,5 @@ function sortable(cls, attr) {
 
 
 export {sortable}; 
+export {hidden1}; 
+export {hidden2}; 
